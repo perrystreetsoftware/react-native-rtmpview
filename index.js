d15b3bdf020e1f3fb3199328517836614bcb561e
Eric Silverberg <eric@scruff.com>
2018-04-08 19:39:14 -0400

    Move code

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //  Created by Eric Silverberg on 2018-04-07
 //  Copyright © 2018 Perry Street Software, Inc. All rights reserved.
 //
-import RtmpView from './RtmpView';
+import RtmpView from './src/RtmpView';
 
 module.exports = { RtmpView };
