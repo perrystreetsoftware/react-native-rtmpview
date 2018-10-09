cf4de0baf4605e1f04b137a6d547f809bb2e7022
Eric Silverberg <eric@scruff.com>
2018-10-09 19:47:02 -0400

    Remove @Override (not used in 0.45) https://github.com/oblador/react-native-vector-icons/issues/515

--- android/src/main/java/com/perrystreetsoftware/RNRtmpViewPackage.java
+++ android/src/main/java/com/perrystreetsoftware/RNRtmpViewPackage.java
@@ -16,7 +16,6 @@ public class RNRtmpViewPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
